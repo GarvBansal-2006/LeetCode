90ed30bd4e2d6943fca87addafaf7634e0998bd1
GarvBansal-2006 <gbansal2006@gmail.com>
2026-07-16 18:10:08 +0530

    Time: 0 ms (100%), Space: 9.2 MB (92.21%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -2,46 +2,27 @@ class Solution {
 public:
     string longestPalindrome(string s) {
         int n = s.size();
-        if (n == 0)
-            return "";
+        int i = 0;
 
-        // dp[i][j] = true if substring s[i..j] is palindrome
-        vector<vector<bool>> dp(n, vector<bool>(n, false));
+        int best_len = 0, best_start = 0;
+        while (i < n) {
+            int l = i, r = i;
+            while (r + 1 < n and s[r + 1] == s[i]) {
+                r++;
+            }
 
-        int maxLen = 1;
-        int start = 0;
+            i = r + 1;
 
-        // every single character is a palindrome
-        for (int i = 0; i < n; i++) {
-            dp[i][i] = true;
-        }
+            while (l - 1 >= 0 and r + 1 < n and s[l - 1] == s[r + 1]) {
+                l--, r++;
+            }
 
-        // check substrings of length 2
-        for (int i = 0; i < n - 1; i++) {
-            if (s[i] == s[i + 1]) {
-                dp[i][i + 1] = true;
-                start = i;
-                maxLen = 2;
+            int len = (r - l) + 1;
+            if (len > best_len) {
+                best_len = len;
+                best_start = l;
             }
         }
-
-        // check substrings of length >= 3
-        for (int len = 3; len <= n; len++) {
-            for (int i = 0; i <= n - len; i++) {
-                int j = i + len - 1;
-                if (s[i] == s[j] && dp[i + 1][j - 1]) {
-                    dp[i][j] = true;
-                    start = i;
-                    maxLen = len;
-                }
-            }
-        }
-
-        // build the result manually (no substr)
-        string result = "";
-        for (int i = start; i < start + maxLen; i++) {
-            result.push_back(s[i]);
-        }
-        return result;
+        return s.substr(best_start, best_len);
     }
 };
