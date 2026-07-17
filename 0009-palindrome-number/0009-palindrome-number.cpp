e963374c25023c69b0b8a8b7fd957b2da7badf91
Garv Bansal <gbansal2006@gmail.com>
2026-07-17 18:12:45 +0530

    Time: 0 ms (100%), Space: 10.9 MB (7.78%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if (x < 0 || (x % 10 == 0 && x != 0))
+        string s = to_string(x);
+        string rev = s;
+
+        reverse(rev.begin(), rev.end());
+
+        if (s != rev)
             return false;
-
-        int reversed = 0;
-        while (x > reversed) {
-            reversed = reversed * 10 + x % 10;
-            x /= 10;
-        }
-
-        return (x == reversed || x == reversed / 10);
+        return true;
     }
 };
