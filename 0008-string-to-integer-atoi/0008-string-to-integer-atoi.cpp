eb6b3431f8ec54f41baa5eac3b3829d0890f67f2
Garv Bansal <gbansal2006@gmail.com>
2026-07-17 17:39:25 +0530

    Time: 0 ms (100%), Space: 9 MB (82.24%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -3,19 +3,23 @@ public:
     int myAtoi(string s) {
         int i = 0, n = s.size();
 
+        // 1. Skip leading spaces
         while (i < n && s[i] == ' ')
             i++;
 
+        // 2. Handle sign
         int sign = 1;
         if (i < n && (s[i] == '+' || s[i] == '-')) {
             sign = (s[i] == '-') ? -1 : 1;
             i++;
         }
 
+        // 3. Convert number with overflow check
         int num = 0;
         while (i < n && isdigit(s[i])) {
             int digit = s[i] - '0';
 
+            // Check for overflow BEFORE adding digit
             if (num > (INT_MAX - digit) / 10) {
                 return (sign == 1) ? INT_MAX : INT_MIN;
             }
