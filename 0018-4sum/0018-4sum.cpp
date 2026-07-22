ecf924572dcc82a11c2127636c78fd02229d6b97
Garv Bansal <gbansal2006@gmail.com>
2026-07-22 21:45:52 +0530

    Time: 11 ms (95.99%), Space: 17.4 MB (84.73%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -6,10 +6,10 @@ public:
         int n = nums.size();
 
         for (int i = 0; i < n; i++) {
-            if (i > 0 && nums[i] == nums[i-1]) continue; 
+            if (i > 0 && nums[i] == nums[i-1]) continue; // skip duplicate i
             
             for (int j = i + 1; j < n; j++) {
-                if (j > i + 1 && nums[j] == nums[j-1]) continue; 
+                if (j > i + 1 && nums[j] == nums[j-1]) continue; // skip duplicate j
                 
                 long long newTarget = (long long)target - nums[i] - nums[j];
                 int left = j + 1, right = n - 1;
@@ -20,6 +20,7 @@ public:
                     if (sum == newTarget) {
                         ans.push_back({nums[i], nums[j], nums[left], nums[right]});
 
+                        // skip duplicates for left and right
                         while (left < right && nums[left] == nums[left+1]) left++;
                         while (left < right && nums[right] == nums[right-1]) right--;
 
