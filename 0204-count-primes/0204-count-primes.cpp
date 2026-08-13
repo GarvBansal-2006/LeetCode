d199fa2a90944e6e98ef94990abe7f5b191c5b97
Garv Bansal <gbansal2006@gmail.com>
2026-08-13 23:55:15 +0530

    Time: 108 ms (97.23%), Space: 12.8 MB (23.41%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,20 +1,35 @@
 class Solution {
+    bool isSet(const vector<unsigned int>& bits, int x) {
+        return bits[x >> 5] & (1u << (x & 31));
+    }
+
+    void clearBit(vector<unsigned int>& bits, int x) {
+        bits[x >> 5] &= ~(1u << (x & 31));
+    }
+
 public:
     int countPrimes(int n) {
         if (n <= 2)
             return 0;
 
-        vector<bool> isPrime(n, true);
-        isPrime[0] = isPrime[1] = false;
+        vector<unsigned int> isPrime((n >> 5) + 1, ~0u);
+
+        clearBit(isPrime, 0);
+        clearBit(isPrime, 1);
 
         for (int i = 2; i * i < n; i++) {
-            if (isPrime[i]) {
+            if (isSet(isPrime, i)) {
                 for (int j = i * i; j < n; j += i) {
-                    isPrime[j] = false;
+                    clearBit(isPrime, j);
                 }
             }
         }
 
-        return count(isPrime.begin(), isPrime.end(), true);
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (isSet(isPrime, i))
+                count++;
+        }
+        return count;
     }
 };
